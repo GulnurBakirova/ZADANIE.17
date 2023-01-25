578041408d84a58f8ae85e5d948eaece4c3b5ac2
array <bakirova0993@gmail.com>
2023-01-25 18:58:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Sheep sheep = new Sheep(30,2,"Gisar","Masha");
+        Horse horse = new Horse(150,4,"Mustang","Orhan");
+        Cow cow = new Cow(100,3,"Hilend","Burenka");
+
+        Sheep [] sheeps = new Sheep[]{
+                new []};
+        String [] cows = new String[]{"Avrora","Belka","Burenka","Galka","Assol"};
+        String [] horses = new String[]{"Orhan","Gerkules"};
+
+        Farm farm = new Farm("Sh entuziastov 7");
+    }
+}
